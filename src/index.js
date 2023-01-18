2d79ee0858048fd28469712a1baf87663c7da6cc
Ben Siebert <bensie_007@icloud.com>
2023-01-18 13:33:31 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const nodeFetch = require('node-fetch');
+
+const app = express();
+
+app.get("/:postal", async (req, res) => {
+    const postal = req.params.postal;
+
+    const fetchRes = await nodeFetch(process.env.API_BASE + "?codes=" + postal, {
+        method: "GET",
+        headers: {
+            "apikey": process.env.API_KEY
+        }
+    });
+
+
+    const data = await fetchRes.json();
+
+    const cityName = data.results[new String(postal)][0].city;
+
+    res.status(200);
+
+    res.json({
+        "city": cityName
+    });
+
+    res.end();
+    
+    
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("> Ready on port 3000")
+})
